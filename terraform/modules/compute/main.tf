7dcf2db6e9844a877fc4364d814ee51bd7caa1af
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-08-27 00:25:05 +1000

    Small space

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -2,6 +2,7 @@ resource "aws_ecr_repository" "ecr" {
   name = "${var.repository_name}"
   tags = "${var.tags}"
 }
+
 resource "aws_ecr_lifecycle_policy" "ecrpolicy" {
   repository = "${aws_ecr_repository.ecr.name}"
 
