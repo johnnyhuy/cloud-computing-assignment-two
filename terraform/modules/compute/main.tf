5375bf527ebfc4ab4654c060b998023ccb58ed6e
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-08-27 00:24:33 +1000

    Added lifecycle policy

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -2,3 +2,25 @@ resource "aws_ecr_repository" "ecr" {
   name = "${var.repository_name}"
   tags = "${var.tags}"
 }
+resource "aws_ecr_lifecycle_policy" "ecrpolicy" {
+  repository = "${aws_ecr_repository.ecr.name}"
+
+  policy = <<EOF
+{
+"rules": [
+  {
+    "action": {
+      "type": "expire"
+    },
+    "selection": {
+      "countType": "imageCountMoreThan",
+      "countNumber": 8,
+      "tagStatus": "any"
+    },
+    "description": "Remove old images.",
+    "rulePriority": 1
+  }
+]
+}
+  EOF
+}
