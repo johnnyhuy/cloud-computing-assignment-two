0628db1d0a26ac081b8adc2758385ac89795ec68
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-08-27 21:55:18 +1000

    Push an GKE with Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = "ap-southeast-2"
 }
 
+provider "google" {
+  project     = "stayapp"
+  region      = "australia-southeast1"
+  zone        = "australia-southeast1-c"
+}
+
 terraform {
   backend "s3" {
     bucket = "stayapp-terraform"
