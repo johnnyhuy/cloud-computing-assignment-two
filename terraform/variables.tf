1f9782854db212efd472860231677d06b712c02b
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-08-27 22:24:55 +1000

    Split up AWS and GCP

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "repository_name" {
-  description = "The suffix path of the registry url."
-  type = "string"
-}
-
-variable "tags" {
-  description = "Tags for all the resources"
-  type = "map"
-}
