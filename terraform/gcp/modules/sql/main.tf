ab64c3ede8f9a51a94d5d0398504ee3a2a2d7acd
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-09-02 21:16:58 +1000

    User host

--- terraform/gcp/modules/sql/main.tf
+++ terraform/gcp/modules/sql/main.tf
@@ -10,10 +10,11 @@ resource "google_sql_database_instance" "stayapp" {
 resource "google_sql_user" "users" {
   name     = "${var.sql_username}"
   instance = "${google_sql_database_instance.stayapp.name}"
+  host     = "${google_sql_database_instance.stayapp.host}"
   password = "${var.sql_password}"
 }
 
 resource "google_sql_database" "database" {
-    name = "stayapp"
-    instance = "${google_sql_database_instance.stayapp.name}"
+  name     = "stayapp"
+  instance = "${google_sql_database_instance.stayapp.name}"
 }
