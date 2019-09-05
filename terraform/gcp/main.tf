a45b404bcc903ea26ce2e335fe93a2edd26ce00d
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-09-05 20:21:49 +1000

    Added networking deployment

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -17,6 +17,15 @@ module "compute" {
   service_account = "${var.service_account}"
 }
 
+module "networking" {
+  source = "./modules/networking"
+  cloudflare_email = "${var.cloudflare_email}"
+  cloudflare_token = "${var.cloudflare_token}"
+  cloudflare_record_domain = "${var.cloudflare_record_domain}"
+  cloudflare_record_name = "${var.cloudflare_record_name}"
+  cloudflare_record_ip_address = "${var.cloudflare_record_ip_address}"
+}
+
 module "sql" {
   source       = "./modules/sql"
   tags         = "${var.tags}"
