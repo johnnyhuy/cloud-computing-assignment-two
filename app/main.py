0f1910d9be13ab9accf3e21090a86be80e496537
goran-stojkoski <g.stojkoski@gmail.com>
2019-09-08 23:54:57 +1000

    added classes for object parsing and suburb. Also added constants.py. Database for crimes and suburbs has also been created. 9 tables were created in total

--- app/main.py
+++ app/main.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template
 import json
+from app.suburb import Suburb
+from app.object_populator import ObjectPopulator
 
 app = Flask(__name__)
 
+with open('./static/response_1567049463301.json') as json_file:
+    listings = json.load(json_file)
 
 @app.route("/")
 def index():
-    with open('./static/response_1567049463301.json') as json_file:
-        listings = json.load(json_file)
-    return render_template('index.html',listings=listings)
+    return render_template('index.html', listings=listings)
 
 
-@app.route("/property")
-def property():
-    return render_template('property.html')
+@app.route("/listing/<property_id>")
+def listing(property_id):
+    for p in listings:
+        if str(p['listing']['id']) == property_id:
+            suburb = Suburb(['propertyDetails']['suburb'])
+            ObjectPopulator.populate_suburb_object(suburb)
+            return render_template('listing.html', p=p,suburb=suburb)
+    return "Property Not Found"
 
 
 if __name__ == "__main__":
