a84318502f2b7816389e5946933ca70015d27696
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-09-21 11:54:58 +1000

    Removed CF vars

--- terraform/gcp/modules/networking/variables.tf
+++ terraform/gcp/modules/networking/variables.tf
@@ -1,13 +1,3 @@
-variable "cloudflare_email" {
-  description = "Cloudflare email used to auth against their API"
-  type = "string"
-}
-
-variable "cloudflare_token" {
-  description = "Cloudflare token used to auth against their API"
-  type = "string"
-}
-
 variable "cloudflare_record_domain" {
   description = "Cloudflare domain zone"
   type = "string"
