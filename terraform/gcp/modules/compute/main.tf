dc16fa120bbf1a30c1ee7c52e49f1d742582f88e
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-09-22 09:00:58 +1000

    Removed zone

--- terraform/gcp/modules/compute/main.tf
+++ terraform/gcp/modules/compute/main.tf
@@ -17,7 +17,7 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "stayapp-node-pool"
-  location   = "australia-southeast1-a"
+  location   = "australia-southeast1"
   cluster    = "${google_container_cluster.primary.name}"
   node_count = 1
 
