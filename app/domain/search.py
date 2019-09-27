caabafc43b8c34cbf2282b4c91c450df43cc4cd4
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-09-27 15:51:21 +1000

    Refactored the search class

--- app/domain/search.py
+++ app/domain/search.py
@@ -5,26 +5,31 @@ import domain.constants as domain_constants
 
 class Search:
 
-    def __init__(self, domain_api_token,api_query):
-        self.token = domain_api_token
-        self.api_query = api_query
+    def __init__(self, api_token):
+        """
+        Search class used to fetch data from the Domain API.
 
-        # Appending to header constant to complete header variable
+        :param api_token: API key from the Domain API
+        """
+
+        self.token = api_token
+        self.headers = domain_constants.SEARCH_PROPERTIES_HEADERS
         domain_constants.SEARCH_PROPERTIES_HEADERS['Authorization'] = 'Bearer ' + self.token
 
-        headers = domain_constants.SEARCH_PROPERTIES_HEADERS
-        data = self.api_query
+    def get_residential(self, api_query):
+        """
+        Get residential data from the API
+
+        :param api_query: JSON query to get content from the API
+        """
 
         try:
             response = requests.post(
                 domain_constants.LISTINGS_SEARCH_URL,
-                headers=headers,
-                data=data
+                headers=self.headers,
+                data=json.dumps(api_query)
             )
 
-            self.json_data = response.json()
-        except Error as e:
+            return response.json()
+        except requests.exceptions.HTTPError as e:
             print(domain_constants.READ_API_ERROR, e)
-
-    def get_data(self):
-        return self.json_data
