07759ffacbd5134c7ce87e784665d5ea4a63744b
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-10-01 18:57:24 +1000

    Use PyCharm debugging

--- app/main.py
+++ app/main.py
@@ -2,29 +2,29 @@ from fastapi import FastAPI, Form, HTTPException
 from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
-import ptvsd
+import pydevd_pycharm
 import os
-import json
 from location import Suburb, Council, State
 from domain.search import Search
 from data_grabber import SuburbData, CouncilData, StateData, FeesData, DomainAccessToken
 from stats import GraphBuilder
-from pydantic import BaseModel
-
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
-if (os.getenv('ENVIRONMENT', 'production') == 'development'):
-    ptvsd.enable_attach(address=('0.0.0.0', 8080))
+if os.getenv('ENVIRONMENT', 'production') == 'development':
+    pydevd_pycharm.settrace('host.docker.internal', port=8081, stdoutToServer=True, stderrToServer=True)
+
 
 domain_access_token = DomainAccessToken().get_token()
 search = Search(domain_access_token)
 
+
 @app.get('/')
 def index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
+
 
 @app.post('/listings/')
 def index(
@@ -61,6 +61,7 @@ def index(
         }
     )
 
+
 @app.get('/listing/{property_id}')
 def listing(request: Request, property_id: int):
     listing = search.get_residential_listing(property_id)
@@ -87,7 +88,7 @@ def listing(request: Request, property_id: int):
         fees = FeesData(price_estimate)
 
         return render_template('listing.html', listing=listing, suburb=suburb, council=council, state=state,
-                                crime_fig_url=crime_fig_url, fees=fees)
+                               crime_fig_url=crime_fig_url, fees=fees)
     else:
         return templates.TemplateResponse(
             'listing.html',
