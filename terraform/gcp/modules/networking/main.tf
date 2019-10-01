2e641bf076da23e29790d907416e0588cffe421e
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-10-01 19:49:00 +1000

    Actually fix CF problem

--- terraform/gcp/modules/networking/main.tf
+++ terraform/gcp/modules/networking/main.tf
@@ -2,9 +2,14 @@ resource "google_compute_address" "ip_address" {
   name = "stayapp-static-ip"
 }
 
-resource "cloudflare_record" "single_a" {
-  domain = "${var.cloudflare_record_domain}"
+resource "cloudflare_zone" "stayapp_zone" {
+    zone = "${var.cloudflare_record_domain}"
+}
+
+resource "cloudflare_record" "stayapp_record" {
+  zone_id = "${cloudflare_zone.stayapp_zone.id}"
   name   = "${var.cloudflare_record_name}"
   value  = "${google_compute_address.ip_address.address}"
   type   = "A"
+  ttl = 3600
 }
