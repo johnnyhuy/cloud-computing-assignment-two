8de53085f4ac43fceed26c7b40c5a7027701d78f
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-10-05 06:37:01 +1000

    Fix up listing page

--- app/main.py
+++ app/main.py
@@ -23,12 +23,12 @@ domain_access_token = DomainAccessToken().get_token()
 search = Search(domain_access_token)
 
 
-@app.get('/')
+@app.get('/', name='home')
 def index(request: Request):
     return templates.TemplateResponse('index.html', {'request': request})
 
 
-@app.post('/listings/')
+@app.post('/listings/', name='get_listings')
 def index(
     request: Request,
     suburb_name: str = Form(...),
@@ -64,11 +64,11 @@ def index(
     )
 
 
-@app.get('/listing/{property_id}')
+@app.get('/listing/{property_id}', name='get_listing')
 def listing(request: Request, property_id: int):
     listing = search.get_residential_listing(property_id)
 
-    if (listing.get('message') == 'Not Found'):
+    if listing.get('message') == 'Not Found':
         raise HTTPException(status_code=404, detail="Property not found")
 
     suburb_name = listing.get('addressParts').get('suburb')
@@ -81,25 +81,56 @@ def listing(request: Request, property_id: int):
     state_data = StateData('Victoria')
     state = State(state_data.get_data())
 
-    crime = None
     crime = GraphBuilder(state, council, suburb)
     crime_fig_url = crime.get_url()
 
-    if request.method == 'POST':
-        price_estimate = request.form.get('price_estimate')
-        fees = FeesData(price_estimate)
+    return templates.TemplateResponse(
+        'listing.html',
+        {
+            'request': request,
+            'listing': listing,
+            'suburb': suburb,
+            'council': council,
+            'state': state,
+            'crime_fig_url': crime_fig_url
+        }
+    )
 
-        return render_template('listing.html', listing=listing, suburb=suburb, council=council, state=state,
-                               crime_fig_url=crime_fig_url, fees=fees)
-    else:
-        return templates.TemplateResponse(
-            'listing.html',
-            {
-                'request': request,
-                'listing': listing,
-                'suburb': suburb,
-                'council': council,
-                'state': state,
-                'crime_fig_url': crime_fig_url
-            }
-        )
+
+@app.post('/listing/{property_id}/price', name='get_price')
+def listing(
+    request: Request,
+    property_id: int,
+    price_estimate: str = Form(...)
+):
+    listing = search.get_residential_listing(property_id)
+
+    if listing.get('message') == 'Not Found':
+        raise HTTPException(status_code=404, detail="Property not found")
+
+    suburb_name = listing.get('addressParts').get('suburb')
+    suburb_data = SuburbData(suburb_name, domain_access_token)
+    suburb = Suburb(suburb_data.get_data())
+
+    council_data = CouncilData(suburb.council_name)
+    council = Council(council_data.get_data())
+
+    state_data = StateData('Victoria')
+    state = State(state_data.get_data())
+
+    crime = GraphBuilder(state, council, suburb)
+    crime_fig_url = crime.get_url()
+
+    fees = FeesData(price_estimate)
+
+    return templates.TemplateResponse(
+        'price.html',
+        {
+            'listing': listing,
+            'suburb': suburb,
+            'council': council,
+            'state': state,
+            'crime_fig_url': crime_fig_url,
+            'fees': fees
+        }
+    )
